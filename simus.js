30faeda853f6f51df6e09636a76398a8020db717
Matheus Avellar <matheusavellar@dcc.ufrj.br>
2019-09-13 10:54:07 -0300

    Rewrite parser with trees

--- simus.js
+++ simus.js
@@ -118,6 +118,10 @@ ${err.desc}
     if(!this.error_list.length) {
       return 0;
     }
+    // If there were any errors, report them
+    if(this.error_list.length > 0) {
+      this.callError(source.split("\n"));
+    }
     // Otherwise, if there were any errors
     this.reset();
     return -1;
@@ -170,6 +174,13 @@ ${err.desc}
     let ch = " ";         // Current character
     let word;  // Current word
 
+    let sim_instance = this;
+    function pushLexeme(lxm) {
+      if(!sim_instance.lexemes[sim_instance.cursor.line])
+        sim_instance.lexemes[sim_instance.cursor.line] = [];
+      sim_instance.lexemes[sim_instance.cursor.line].push(lxm);
+    }
+
     // While there's anything in this line
     while(line.length > 0 || ch.length) {
       // Reset word from previous iteration
@@ -182,9 +193,23 @@ ${err.desc}
       if(ch.match(ALPHA)) {
         // Consume letters or underscore to form a word
         [word,ch,line] = this.consume(ALPHA,ch,line);
-        //if(+ch == 3) debugger
-        // We found a word, let's save it and continue
-        this.lexemes.push({
+        // If there's a colon afterwards, it's a label
+        if(ch === ':') {
+          pushLexeme({
+            typ: "label",
+            val: word,
+            pos: {
+              line: this.cursor.line,
+              column: this.cursor.column - word.length
+            },
+            len: word.length
+          });
+          // Get next character
+          [ch,line] = this.readNext(line);
+          continue;
+        }
+        // We found an ID, let's save it and continue
+        pushLexeme({
           typ: "id",
           val: word,
           pos: {
@@ -197,20 +222,35 @@ ${err.desc}
       }
       // If we found an immediate value (#<number>)
       else if(ch.match(HEX) || ch.match(/#/)) {
-        let _type = (ch.match(HEX)) ? "num" : "imm";
+        let _type = (ch.match(HEX)) ? "number" : "immediate value";
         let _first_ch = ch;
         // Get next character
         [ch,line] = this.readNext(line);
         // Consume 0-9 and A-F
         [word,ch,line] = this.consume(HEX,ch,line);
+        // If there's no number there
+        if(_first_ch === '#' && !word.trim().length) {
+          // Throw error
+          this.error_list.push({
+            cat: "Syntax",
+            desc: `Missing numeric value after '#'`,
+            pos: {
+              line: this.cursor.line,
+              column: this.cursor.column - 1
+            },
+            len: 1
+          });
+        }
+        // Otherwise, prepend first read char (either a '#' or a number) to word
         word = _first_ch + word;
 
-        if(ch === 'H' || ch === 'h') {
+        // Check for an H at the end, specifying it's in base 16
+        if(ch.toLowerCase() === 'h') {
           word += "H";
           // Read next character and leave it on 'ch'
           [ch,line] = this.readNext(line);
         }
-        // If there's A-F inside the number but it doesn't have H after it
+        // If there's A-F inside the number but it doesn't have H at the end
         else if(word.match(/[A-F]/)) {
           // Throw error
           this.error_list.push({
@@ -226,7 +266,7 @@ ${err.desc}
           return;
         }
         // We found a word, let's save it and continue
-        this.lexemes.push({
+        pushLexeme({
           typ: _type,  // This can be "num" or "imm"
           val: word,
           pos: {
@@ -263,8 +303,8 @@ ${err.desc}
           // Read next character and leave it on 'ch'
           [ch,line] = this.readNext(line);
           // We found a word, let's save it and continue
-          this.lexemes.push({
-            typ: "str",
+          pushLexeme({
+            typ: "string",
             val: word,
             pos: {
               line: this.cursor.line,
@@ -277,19 +317,16 @@ ${err.desc}
       }
       // Otherwise, let's get whatever character we found
       else switch(ch) {
-        default: {
-          // If it's empty, ready next and continue
-          if(!ch.trim().length) {
-            [ch,line] = this.readNext(line);
-            continue;
-          }
-          // Otherwise, push it as it is
-          this.lexemes.push({
-            typ: "chr",
+        case "@":
+        case "+":
+        case ",": {
+          // Push these as they are
+          pushLexeme({
+            typ: ch,
             val: ch,
             pos: {
               line: this.cursor.line,
-              column: this.cursor.column - 1
+              column: this.cursor.column
             },
             len: 1
           });
@@ -297,12 +334,153 @@ ${err.desc}
           [ch,line] = this.readNext(line);
           continue;
         }
+        default: {
+          // If it's empty, ready next and continue
+          if(!ch.trim().length) {
+            [ch,line] = this.readNext(line);
+            continue;
+          }
+          // Otherwise, throw error
+          this.error_list.push({
+            cat: "Syntax",
+            desc: `Unexpected character '${ch}'`,
+            pos: {
+              line: this.cursor.line,
+              column: this.cursor.column
+            },
+            len: 1
+          });
+          // Ignore rest of the line
+          return;
+        }
       }
     }
   }
 
   parser() {
+    // Get first line
+    let token;
+    let line;
+    let sim_instance = this;
 
+    // While there's lines to be parsed
+    while(this.lexemes.length) {
+      // Get next line
+      line = this.lexemes.shift();
+      // Get next token from line
+      token = getNextToken();
+      // While line isn't empty, parse
+      V();
+    }
+
+    // Helpers to prevent "cannot read property of undefined"
+    function getTokenVal() {
+      return token ? token.val : "";
+    }
+    function getTokenTyp() {
+      return token ? token.typ : "";
+    }
+    function getNextToken() {
+      return line ? line.shift() : "";
+    }
+
+    // Given something, match it to the current token
+    function token_match(expected, loc) {
+      if(!token) return;
+      // If we found what we expected
+      if(token.typ === expected) {
+        // Save token to intermediate list
+        /* FIXME: Differentiate each command inside intermediate list */
+        sim_instance.intermediate.push(token);
+        // Get next token
+        token = getNextToken();
+      }
+      // If it's not what we expected
+      else {
+        // Throw error
+        sim_instance.error_list.push({
+          cat: "Parse",
+          desc: `Expected '${expected}', found '${token.typ}' (at ${loc})`,
+          pos: token.pos,
+          len: token.len
+        });
+      }
+    }
+
+    /* V() */
+    /* V => L() C()*/
+    /* L => [LABEL:] L() | "" */
+    /* C => I() A() */
+    /* I => [CMD] (EQU) */
+    /* A => [@#0-9A-Z] , A() | "" */
+    function V() {
+      // Labels
+      L();
+      // Command
+      C();
+    }
+
+    function L() {
+      // Consume all labels
+      while(getTokenTyp() === "label") {
+        token_match("label", "L");
+      }
+    }
+
+    function C() {
+      // Instruction / keyword
+      I();
+      // Argument
+      A();
+    }
+
+    function I() {
+      // Match an ID
+      token_match("id", "I");
+      // If "<id> EQU"...
+      if(getTokenVal() === "EQU") {
+        // Then match the EQU
+        token_match("id", "I");
+        // And a number (i.e. <id> EQU <number>)
+        token_match("number", "I");
+      }
+    }
+
+    function A(mandatory) {
+      // Check if we have any argument
+      let _current_type = getTokenTyp();
+      switch(_current_type) {
+        // Arguments can be IDs, numbers, immediate values...
+        case "id":
+        case "number":
+        case "immediate value":
+          token_match(_current_type, "A");
+          break;
+        // Or indirect values
+        case "@":
+          token_match("@");
+          switch(getTokenTyp()) {
+            case "number":
+              token_match("number", "A");
+              break;
+            default:
+              token_match("id", "A");
+              break;
+          }
+          break;
+        default:
+          if(mandatory)
+            token_match("argument", "A");
+          break;
+      }
+      // Check if there's a list of arguments
+      switch(getTokenTyp()) {
+        case ",":
+          token_match(",", "A");
+          A(true /*if we found a comma, we MUST have more arguments*/);
+          break;
+      }
+    }
   }
 
   setMemory(index,value) {
